9e3cb5216e90d7cf8048c46d0b07e2aeec483fbc
TagliatL <maurer.paul@ymail.com>
2014-11-21 14:45:16 +0100

    wah c'est beau le background et les particles toussa + particles plus custom

---
+++ JS/Objects/InvisibleWall.js
@@ -0,0 +1,22 @@
+var InvisibleWall = function InvisibleWall(params){
+    this.tag = "Emitter";
+    this.mesh = BABYLON.Mesh.CreateBox(params.name, 1, params.scene.scene);
+    this.mesh.position = params.position;
+    this.mesh.scaling = new BABYLON.Vector3(1,15, 10);
+    this.mesh.isVisible = false;
+
+    Particle({
+        name : "sky",
+        scene : params.scene,
+        emitter : this,
+        size : 1,
+        rate : 80,
+        life : 8 ,
+        minEmitBox : new BABYLON.Vector3(0, 3, 0),
+        maxEmitBox : new BABYLON.Vector3(0, -3, 0),
+        img : "img/space.png",
+        dir1 : new BABYLON.Vector3(-100, 0, 0),
+        dir2 : new BABYLON.Vector3(-100, 0, 0)
+    });
+};
+extend(InvisibleWall, GameObject);
