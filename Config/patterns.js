0fa18f1f27ecbd8c3756e8aa099eb9b7b8f2d608
BobbyWatson <louis.arthur.cabrera@gmail.com>
2014-12-05 14:57:51 +0100

    ok la regression...Lol

--- Config/patterns.js
+++ Config/patterns.js
@@ -1,41 +1,11 @@
 var patterns = [
-	//mines
+	//Pattern 1
 	[
-		{
-			name : Mine,
-			params : {
-				position : {
-					x 	: 0,
-					y	: 4,
-					z	: 0
-				}
-			}
-		},
-		{
-			name : Mine,
-			params : {	
-				position : {
-					x 	: 2,
-					y	: 2,
-					z	: 0
-				}
-			}
-		},
 		{
 			name : Mine,
 			params : {	
 				position : {
 					x 	: 4,
-					y	: 4,
-					z	: 0
-				}
-			}
-		},
-		{
-			name : Mine,
-			params : {	
-				position : {
-					x 	: -2,
 					y	: 2,
 					z	: 0
 				}
@@ -45,79 +15,317 @@ var patterns = [
 			name : Mine,
 			params : {	
 				position : {
-					x 	: -4,
+					x 	: 5,
 					y	: 4,
 					z	: 0
 				}
 			}
-		}
-	],
-    //Another
-    [
-        {
-            name : Spike,
-            params : {
-                orientation : "top",
-                length : 10,
-                position : {x:0,y:5,z:0}
-            }
-        }
-    ],
-    //SpikeBottom
-    [
-        {
-            name : Spike,
-            params : {
-                orientation : "bottom",
-                length : 10,
-                position : {x:0,y:1,z:0}
-            }
-        }
-    ],
-    //MovingWall
-    [
-        {
-            name : MovingWall,
-            params : {
-                length : 10,
-                position : {x:0,y:3,z:0}
-            }
-        }
-    ],
-    //MovingMine
-    [
-        {
-            name : MovingMine,
-            params : {
-                position : {
-                    x 	: 0,
-                    y	: 5,
-                    z	: 0
-                }
-            }
-        }
-    ],
-    //coins
-    [
-    	{
-			name : Collectible,
+		},
+		{
+			name : Mine,
 			params : {	
 				position : {
-					x 	: -2,
+					x 	: 6,
+					y	: 6,
+					z	: 0
+				}
+			}
+		},
+		{
+			name : Mine,
+			params : {	
+				position : {
+					x 	: 7,
+					y	: 8,
+					z	: 0
+				}
+			}
+		},
+		{
+			name : Mine,
+			params : {	
+				position : {
+					x 	: 8,
 					y	: 10,
 					z	: 0
 				}
 			}
 		},
 		{
-			name : Collectible,
+			name : Mine,
 			params : {	
 				position : {
-					x 	: 2,
+					x 	: 18,
+					y	: 14,
+					z	: 0
+				}
+			}
+		},
+		{
+			name : Mine,
+			params : {	
+				position : {
+					x 	: 19,
+					y	: 12,
+					z	: 0
+				}
+			}
+		},
+		{
+			name : Mine,
+			params : {	
+				position : {
+					x 	: 20,
 					y	: 10,
 					z	: 0
 				}
 			}
+		},		
+		{
+			name : Mine,
+			params : {	
+				position : {
+					x 	: 21,
+					y	: 8,
+					z	: 0
+				}
+			}
+		},		
+		{
+			name : Mine,
+			params : {	
+				position : {
+					x 	: 22,
+					y	: 6,
+					z	: 0
+				}
+			}
+		},
+		{
+			name : Collectible,
+			params : {
+				position : {
+					x 	: 15,
+					y	: 7,
+					z 	: 0
+				}
+			}
+		},
+		{
+			name : Collectible,
+			params : {
+				position : {
+					x 	: 15,
+					y	: 4,
+					z 	: 0
+				}
+			}
+		},
+		{
+			name : Collectible,
+			params : {
+				position : {
+					x 	: 2,
+					y	: 2,
+					z 	: 0
+				}
+			}
 		}
-    ]
+	],
+	//Patern 3
+	[
+		{
+			name : MovingMine,
+			params : {
+				position : {
+					x 	: 5,
+					y	: 7,
+					z 	: 0
+				}
+			}
+		},
+		{
+			name : Collectible,
+			params : {
+				position : {
+					x 	: 8,
+					y	: 9,
+					z 	: 0
+				}
+			}
+		},
+		{
+			name : Collectible,
+			params : {
+				position : {
+					x 	: 10,
+					y	: 8,
+					z 	: 0
+				}
+			}
+		},
+		{
+			name : Collectible,
+			params : {
+				position : {
+					x 	: 12,
+					y	: 7,
+					z 	: 0
+				}
+			}
+		},		
+		{
+			name : Collectible,
+			params : {
+				position : {
+					x 	: 10,
+					y	: 6,
+					z 	: 0
+				}
+			}
+		},
+		{
+			name : Collectible,
+			params : {
+				position : {
+					x 	: 8,
+					y	: 5,
+					z 	: 0
+				}
+			}
+		},
+		{
+			name : Mine,
+			params : {
+				position : {
+					x 	: 23,
+					y	: 14.5,
+					z 	: 0
+				}
+			}
+		},
+		{
+			name : Mine,
+			params : {
+				position : {
+					x 	: 23,
+					y	: 1.5,
+					z 	: 0
+				}
+			}
+		}
+	],
+	//Patern 8
+	[
+		{
+			name : Spike,
+			params : {
+				position : {
+					x : 2,
+					y : 0,
+					z : 0
+				},
+				length : 10,
+				orientation : "top"
+			}
+		},
+		{
+			name : MovingWall,
+			params : {
+				position : {
+					x : 2,
+					y : 14,
+					z : 0
+				},
+				length : 6
+			}
+		},
+		{
+			name : Spike,
+			params : {
+				position : {
+					x : 10,
+					y : 0,
+					z : 0
+				},
+				length : 10,
+				orientation : "down"
+			}
+		},
+		{
+			name : MovingWall,
+			params : {
+				position : {
+					x : 10,
+					y : 2,
+					z : 0
+				},
+				length : 6
+			}
+		},
+		{
+			name : Spike,
+			params : {
+				position : {
+					x : 18,
+					y : 0,
+					z : 0
+				},
+				length : 10,
+				orientation : "top"
+			}
+		},
+		{
+			name : MovingWall,
+			params : {
+				position : {
+					x : 18,
+					y : 14,
+					z : 0
+				},
+				length : 6
+			}
+		},
+		{
+			name : Mine,
+			params : {
+				position : {
+					x : 11,
+					y : 15,
+					z : 0
+				},
+			}
+		},
+		{
+			name : Collectible,
+			params : {
+				position : {
+					x : 2,
+					y : 12,
+					z : 0
+				},
+			}
+		},
+		{
+			name : Collectible,
+			params : {
+				position : {
+					x : 18,
+					y : 12,
+					z : 0
+				},
+			}
+		},
+		{
+			name : Collectible,
+			params : {
+				position : {
+					x : 10,
+					y : 4,
+					z : 0
+				},
+			}
+		}
+	]
+
 ]
